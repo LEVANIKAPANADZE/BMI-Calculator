f5bccb51258226abccd09a969bf671aeb0e88cc8
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2026-06-16 15:02:05 +0400

    Made components folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "./components/header/page";
+import Footer from "./components/footer/page";
 
 export const metadata: Metadata = {
   title: "BMI Calculator",
@@ -13,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
