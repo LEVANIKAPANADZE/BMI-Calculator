88b7b6938a25f56827cf13f5e502a085f8dd5fa7
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2026-07-01 00:01:49 +0400

    Made footer structure

--- app/components/footer/page.tsx
+++ app/components/footer/page.tsx
@@ -1,3 +1,14 @@
-export default function page() {
-  return <footer>footer</footer>;
+export default function Footer() {
+  return (
+    <footer>
+      <h2>BMI Calculator</h2>
+
+      <p>
+        Calculate your Body Mass Index and receive simple health insights to
+        help you better understand your overall wellness.
+      </p>
+
+      <small>© 2026 Levani Kapanadze. All rights reserved.</small>
+    </footer>
+  );
 }
