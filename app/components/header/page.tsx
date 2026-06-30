7f044cf10dca7af174fcefa7f5b346f384b8b535
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2026-06-30 23:31:48 +0400

    Made data.json and header structure

--- app/components/header/page.tsx
+++ app/components/header/page.tsx
@@ -1,3 +1,13 @@
-export default function header() {
-  return <header>header</header>;
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <h1>BMI Calculator</h1>
+      </div>
+
+      <nav>
+        <button type="button">🌙</button>
+      </nav>
+    </header>
+  );
 }
